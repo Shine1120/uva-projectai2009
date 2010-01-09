96b959068f33c52c255ca26f8cf3cbb3670762d0
nraiman <nraiman@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-01-09 16:16:30 +0000

    runColorEdge enhanced

--- src/runColorEdge.m
+++ src/runColorEdge.m
@@ -1,13 +1,14 @@
-function [] = runEdge2()
+function [] = runColorEdge()
 
 
     close all
+    
     %windows
-     pathFit = '..\DirtyMoney2010\wholeplusborder\neur05\fit\';
-     pathUnFit = '..\DirtyMoney2010\wholeplusborder\neur05\unfit\';
+    pathFit = '..\DirtyMoney2010\wholeplusborder\neur05\fit\';
+    pathUnFit = '..\DirtyMoney2010\wholeplusborder\neur05\unfit\';
 
-%      pathFit = '..\DirtyMoney2010\whitepatch\neur05\fit\';
-%      pathUnFit = '..\DirtyMoney2010\whitepatch\neur05\unfit\';
+%    pathFit = '..\DirtyMoney2010\whitepatch\neur05\fit\';
+%    pathUnFit = '..\DirtyMoney2010\whitepatch\neur05\unfit\';
 
 %    pathFit = '..\DirtyMoney2010\whole\neur05\fit\';
 %    pathUnFit = '..\DirtyMoney2010\whole\neur05\unfit\';
@@ -30,18 +31,22 @@ function [] = runEdge2()
 
     do = 'edge';
 %    do = 'color';
-%    do = 'edgeColor';
+%    do = 'edge and color';
+
+    histogramBins = 25;
 
     cannyThresh=0.03;
     
+    fprintf('\n\n===========STARTING==========================\n')
+    fprintf('\nstarting to run classification by %s...\n\n',do)
     for loop=1:2
         %loop over fit and unfit
         if loop==1 %fit
             path=pathFit;
-            type='fit'
+            fprintf('processing fit data...\n')
         elseif loop==2 %unfit
             path=pathUnFit;
-            type='unfit'
+            fprintf('processing unfit data...\n')
         end
         
         numTrainImages=1;
@@ -49,6 +54,7 @@ function [] = runEdge2()
         trainResults=[0,0];
         testResults=[0,0];
         
+        fprintf('\tconstructing the test set\n')
         for i = 1:trainImg
             nextImageNameFront = [path 'f' num2str(i,'%01d') '.bmp'];
             nextImageNameRear = [path 'r' num2str(i,'%01d') '.bmp'];
@@ -66,7 +72,7 @@ function [] = runEdge2()
                     frontImageCount = doColor(nextImageFront);
                     rearImageCount  = doColor(nextImageRear);
                 end
-                if strcmp(do,'edgeColor')
+                if strcmp(do,'edge and color')
                     frontImageCount = doEdgeColor(nextImageFront,cannyThresh);
                     rearImageCount  = doEdgeColor(nextImageRear,cannyThresh);
                 end
@@ -77,6 +83,7 @@ function [] = runEdge2()
                 numTrainImages=numTrainImages+1;
             end
         end
+        fprintf('\tconstructing the train set\n')
         for j = trainImg+1:maxNrImg
             nextImageNameFront = [path 'f' num2str(j,'%01d') '.bmp'];
             nextImageNameRear = [path 'r' num2str(j,'%01d') '.bmp'];
@@ -94,7 +101,7 @@ function [] = runEdge2()
                     frontImageCount = doColor(nextImageFront);
                     rearImageCount  = doColor(nextImageRear);
                 end
-                if strcmp(do,'edgeColor')
+                if strcmp(do,'edge and color')
                     frontImageCount = doEdgeColor(nextImageFront,cannyThresh);
                     rearImageCount  = doEdgeColor(nextImageRear,cannyThresh);
                 end
@@ -107,21 +114,34 @@ function [] = runEdge2()
             end
         end
         
-%         figure('Name',type)
-%         plot(trainResults(1,:))
-%         hold on
-%         plot(1:maxNrImg,mean(trainResults(1,:)),'r')
         if loop==1
             trainResultsFit = trainResults;
             testResultsFit = testResults;
-            mR1 = mean(trainResultsFit(1,:));
-            cR1 = cov(trainResultsFit(1,:));
+            meanFit = mean(trainResultsFit(1,:));
+            covFit = cov(trainResultsFit(1,:));
+            %Histogram calculation and plot
+            figure(1)
+            subplot(1,2,1)
+            [hist, binSize,mini] = histConstr(trainResultsFit(1,:),histogramBins);
+            x = 1:histogramBins;
+            x = (x.*binSize)+mini;
+            bar(x,hist);
+            title('Histogram Fit train')
+
         end
         if loop==2
             trainResultsUnfit = trainResults;
             testResultsUnfit = testResults;
-            mR2 = mean(trainResultsUnfit(1,:));
-            cR2 = cov(trainResultsUnfit(1,:));
+            meanUnfit = mean(trainResultsUnfit(1,:));
+            covUnfit = cov(trainResultsUnfit(1,:));
+            %Histogram calculation and plot
+            figure(1)
+            subplot(1,2,2)
+            [hist, binSize,mini] = histConstr(trainResultsUnfit(1,:),histogramBins);
+            x = 1:histogramBins;
+            x = (x.*binSize)+mini;
+            bar(x,hist);
+            title('Histogram Unfit train')
         end
     end
     figure('name','Probabilities')
@@ -130,7 +150,7 @@ function [] = runEdge2()
         %for all edge counts in 1 (fit) do:
         %get probability of x according to mean and covariance of 
         % results 1 (edges of fit bills)
-        probFitBeFit(x) = bigauss(mR1,cR1,testResultsFit(1,x));
+        probFitBeFit(x) = bigauss(meanFit,covFit,testResultsFit(1,x));
     end
     %normalize
     probFitBeFit = probFitBeFit./max(probFitBeFit);
@@ -146,7 +166,7 @@ function [] = runEdge2()
         %for all edge counts in 2 (unfit) do:
         %get probability of x according to mean and covariance of 
         % results 1 (edges of fit bills)
-        probUnfitBeFit(x) = bigauss(mR1,cR1,testResultsUnfit(1,x));
+        probUnfitBeFit(x) = bigauss(meanFit,covFit,testResultsUnfit(1,x));
     end
     probUnfitBeFit = probUnfitBeFit./max(probUnfitBeFit);
     subplot(2,2,2); 
@@ -160,7 +180,7 @@ function [] = runEdge2()
         %for all edge counts in 1 (fit) do:
         %get probability of x according to mean and covariance of 
         % results 2 (edges of unfit bills)
-        probFitBeUnfit(x) = bigauss(mR2,cR2,testResultsFit(1,x));
+        probFitBeUnfit(x) = bigauss(meanUnfit,covUnfit,testResultsFit(1,x));
     end
     probFitBeUnfit = probFitBeUnfit./max(probFitBeUnfit);
     subplot(2,2,3); 
@@ -174,7 +194,7 @@ function [] = runEdge2()
         %for all edge counts in 2 (unfit) do:
         %get probability of x according to mean and covariance of 
         % results 2 (edges of unfit bills)
-        probUnfitBeUnfit(x) = bigauss(mR2,cR2,testResultsUnfit(1,x));
+        probUnfitBeUnfit(x) = bigauss(meanUnfit,covUnfit,testResultsUnfit(1,x));
     end
     probUnfitBeUnfit = probUnfitBeUnfit./max(probUnfitBeUnfit);
     subplot(2,2,4); 
@@ -205,8 +225,13 @@ function [] = runEdge2()
         end
     end
    
-    percentageFitGood = fitGood/(fitGood+fitNotGood)*100
-    percentageUnfitGood = unfitGood/(unfitGood+unfitNotGood)*100
+    percFitGood = fitGood/(fitGood+fitNotGood)*100;
+    percUnfitGood = unfitGood/(unfitGood+unfitNotGood)*100;
+    
+    fprintf('\nresults:\n')
+    fprintf('%6.4g%% of the fit test data is classified good\n',percFitGood)
+    fprintf('%6.4g%% of the unfit test data is classified good\n',percUnfitGood)
+    fprintf('\n===========FINISHED==========================\n')
 
 end
 
@@ -227,4 +252,25 @@ function p = bigauss(mu,c,x)
 	a = 2 * pi * sqrt(det(c));
 	b = (x - mu) * inv(c) * (x - mu)';
 	p = (1/a) * exp(-b/2);
+end
+
+function [histogram,binSize,mini] = histConstr(list,bins)
+
+    mini = min(list);
+    maxi = max(list);
+    diff = maxi-mini;
+    binSize = diff/bins;
+    
+    resultBinned = zeros(size(list));
+    histogram = zeros(bins,1);
+    for i=1:bins
+        mask = list>=((binSize*(i-1))+mini) &...
+            list<=((binSize*(i))+mini);
+
+        resultBinned(mask) = i;
+    end
+    for t=1:size(list,2)
+       %add occurences to bar 
+       histogram(resultBinned(t)) = histogram(resultBinned(t)) + 1;
+    end
 end
