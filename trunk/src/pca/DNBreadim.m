3fe690e54833aef8b692aec1cda91c95610404c6
jturnhout <jturnhout@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-01-12 15:56:39 +0000

    pca src updated

--- trunk/src/pca/DNBreadim.m
+++ trunk/src/pca/DNBreadim.m
@@ -9,3 +9,10 @@ im = im2double(imread(str));
 
 %% reshape into long vector
 im = reshape(im,1,prod(size(im)));
+
+
+%% TODO:
+% - for each bill, subtract the mean of that bill (to get intensity
+% invariance) (results should go down)
+% - for both training and test-set subtract subtract the emperical mean of
+% the training-set before projecting (results should go up)
