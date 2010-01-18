c8f09d70454ff0ba5c7d3044908f58c4b5417eaa
silvia.laura.pintea <silvia.laura.pintea@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-01-18 11:04:02 +0000

    haar update -- with best model for the holdout set

---
+++ trunk/src/haar/plot_features.m
@@ -0,0 +1,31 @@
+%PLOT THE BEST FEATURES FOUND ON A RANDOMLY SELECTED BILL
+%INPUT:
+%       ImgSet              -- the images of the training set saved as a matrix   
+%		best_feature_indexs -- array of indexs corresponding to the best featurees 
+%		rect_patterns       -- the structure containint all possible
+%							   patterns: rectangles(=matrix),pattern_id(=id)	
+%       F                   -- the whole feature structure: x_top, y_top(=top coordinates), 
+%							   pattern_id(=corresponding pattern id), feature_id
+%		side                -- 'rear'/'front' 
+%		T                   -- number of features to be used (not too large :P)
+function plot_features(ImgSet,best_feature_indexs,F,patterns, side, T)
+	%plot the best features on a random image
+	[Ny , Nx , P] = size(ImgSet); %P number of images   
+	Nimage   = randperm(P); %Random image index for printing the result over it
+    rand_Img = ImgSet(: , : , Nimage(1)); %random image for ploting the features
+	figure;imshow(rand_Img,'DisplayRange', [0 255]);hold on;
+	for f = 1:size(best_feature_indexs,2)
+		x_top  = double(F(best_feature_indexs(f)).x_top); 
+		y_top  = double(F(best_feature_indexs(f)).y_top);		
+		width  = size(patterns(F(best_feature_indexs(f)).pattern_id).rectangles,2);
+		height = size(patterns(F(best_feature_indexs(f)).pattern_id).rectangles,1);	
+		h = imshow((patterns(F(best_feature_indexs(f)).pattern_id).rectangles+1).*255/2, ...
+			'XData', [x_top x_top+width],'YData', [y_top y_top+height], 'Border', 'tight', 'DisplayRange', [0 255]);
+		plot(x_top,y_top,'r*');
+	end
+%	h = imshow(rand_Img,'DisplayRange', [0 255]);
+%	set(h,'AlphaData', 0.1)	
+    hold off;
+    title(sprintf('Best %d %s Haar features with Adaboost' , T, side) , 'fontsize' , 13)
+    colormap(gray)
+end
--- trunk/src/haar/save_patterns.m
+++ trunk/src/haar/save_patterns.m
@@ -1,32 +1,32 @@
 function save_patterns(imsize_x, imsize_y)
 	%DEFINE BASIC PATTERNS
-	patterns(1) = struct('pattern_id',1, 'rectangles', [1 -1;1 -1]);
-	patterns(2) = struct('pattern_id',2, 'rectangles', [-1 1;-1 1]);
-	patterns(3) = struct('pattern_id',3, 'rectangles', [-1 -1;1 1]);
-	patterns(4) = struct('pattern_id',4, 'rectangles', [1 1;-1 -1]);
-	patterns(5) = struct('pattern_id',5, 'rectangles', [-1 1;1 -1]);
-	patterns(6) = struct('pattern_id',6, 'rectangles', [1 -1;-1 1]);
-	patterns(7) = struct('pattern_id',7, 'rectangles', [1 -1 1;1 -1 1]);
-	patterns(8) = struct('pattern_id',8, 'rectangles', [-1 1 -1;-1 1 -1]);
-	patterns(9) = struct('pattern_id',9, 'rectangles', [-1 -1;1 1;-1 -1]);
-	patterns(10) = struct('pattern_id',10, 'rectangles', [1 1;-1 -1;1 1]);
+	pre_patterns(1) = struct('pattern_id',1, 'rectangles', [1 -1;1 -1]);
+	pre_patterns(2) = struct('pattern_id',2, 'rectangles', [-1 1;-1 1]);
+	pre_patterns(3) = struct('pattern_id',3, 'rectangles', [-1 -1;1 1]);
+	pre_patterns(4) = struct('pattern_id',4, 'rectangles', [1 1;-1 -1]);
+	pre_patterns(5) = struct('pattern_id',5, 'rectangles', [-1 1;1 -1]);
+	pre_patterns(6) = struct('pattern_id',6, 'rectangles', [1 -1;-1 1]);
+	pre_patterns(7) = struct('pattern_id',7, 'rectangles', [1 -1 1;1 -1 1]);
+	pre_patterns(8) = struct('pattern_id',8, 'rectangles', [-1 1 -1;-1 1 -1]);
+	pre_patterns(9) = struct('pattern_id',9, 'rectangles', [-1 -1;1 1;-1 -1]);
+	pre_patterns(10) = struct('pattern_id',10, 'rectangles', [1 1;-1 -1;1 1]);
 	
-	size_patt = size(patterns,2);
+%	size_patt = size(pre_patterns,2);
 	
 	%RESIZE THE BASIC PATTERNS
-	for i=1:size(patterns,2)
-		width      = size(patterns(i).rectangles,2);
-		height     = size(patterns(i).rectangles,1);
+	for i=1:size(pre_patterns,2)
+		width      = size(pre_patterns(i).rectangles,2);
+		height     = size(pre_patterns(i).rectangles,1);
 		randsize_x = randperm(round(imsize_x/width)-10);
 		randsize_y = randperm(round(imsize_y/height)-10);
 		
-		size_x     = randsize_x(randsize_x>20);
-		size_y     = randsize_y(randsize_y>20);
+		size_x     = randsize_x(randsize_x>10);
+		size_y     = randsize_y(randsize_y>10);
 		number     = min(20, length(size_x)); 
 		number     = min(number, length(size_y));
 		for j=1:number
-			index           = size_patt + number*(i-1)+j;
-			rectangles      = sign(imresize_old(patterns(i).rectangles,[height*size_y(j) width*size_x(j)]));
+			index           = number*(i-1)+j; %size_patt + number*(i-1)+j;
+			rectangles      = sign(imresize_old(pre_patterns(i).rectangles,[height*size_y(j) width*size_x(j)]));
 			patterns(index) = struct('pattern_id',index, 'rectangles', rectangles);
 		end
 	end
