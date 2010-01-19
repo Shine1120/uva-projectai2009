68e4fa9f5516791fd4e3313473715648f394f395
jturnhout <jturnhout@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-01-19 13:31:12 +0000

    pca updated

--- src/pca/preprocessnew.m
+++ src/pca/preprocessnew.m
@@ -1,4 +1,4 @@
-function [front rear labels] = preprocessnew(value)
+function preprocessnew(value)
 % PREPROCESS the money files. It will return all the nodes in a 2d matrix
 
 value
@@ -13,22 +13,43 @@ fitset_rear = dir([fitdir 'r*.bmp']);
 unfitset_front = dir([unfitdir 'f*.bmp']);
 unfitset_rear = dir([unfitdir 'r*.bmp']);
 
-front = []; rear = []; labels = [];
+banknoteSize_front = size(DNBreadim([fitdir fitset_front(1).name]));
+banknoteSize_rear = size(DNBreadim([fitdir fitset_rear(1).name]));
+
+all_money_front = zeros(length(fitset_front)+length(unfitset_front), banknoteSize_front(1)*banknoteSize_front(2));
+all_money_rear = zeros(length(fitset_rear)+length(unfitset_rear), banknoteSize_rear(1)*banknoteSize_rear(2));
+all_labels = [];
+
+
 for i = 1:length(fitset_front)
     banknote = DNBreadim([fitdir fitset_front(i).name]);
-    front = [front; banknote];
-    labels = [labels; 0];
+    all_money_front(i,:) = banknote;
+    all_labels = [all_labels; 0];
 end;
 for i = 1:length(unfitset_front)
     banknote = DNBreadim([unfitdir unfitset_front(i).name]);
-    front = [front; banknote];
-    labels = [labels; 1];
+    all_money_front(i+length(fitset_front),:) = banknote;
+    all_labels = [all_labels; 1];
 end;
+
+save all_money_front.mat all_money_front
+clear all_money_front
+
 for i = 1:length(fitset_rear)
     banknote = DNBreadim([fitdir fitset_rear(i).name]);
-    rear = [rear; banknote];
+    all_money_rear(i,:) = banknote;
 end;
 for i = 1:length(unfitset_rear)
     banknote = DNBreadim([unfitdir unfitset_rear(i).name]);
-    rear = [rear; banknote];
+    all_money_rear(i+length(fitset_rear),:) = banknote;
 end;
+
+save all_money_rear.mat all_money_rear
+clear all_money_rear
+
+save all_labels.mat all_labels
+clear all_labels
+
+
+	 
+	
