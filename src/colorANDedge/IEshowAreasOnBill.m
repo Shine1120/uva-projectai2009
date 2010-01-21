ec7f5934b83c1bab90a6d94b710ec458357ac482
nraiman <nraiman@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-01-21 17:09:29 +0000

    show filled rectangles on bill

--- src/colorANDedge/IEshowAreasOnBill.m
+++ src/colorANDedge/IEshowAreasOnBill.m
@@ -1,38 +1,39 @@
 function [ output_args ] = IEshowAreasOnBill( Xsegs, Ysegs,...
 	doFront,doRear,numberOfMethods,bestModels)
 
-	segsPerSide = Xsegs * Ysegs;
-	segsPerMethod = segsPerSide * (doFront+doRear);
+	segsPerSide		= Xsegs * Ysegs;
+	segsPerMethod	= segsPerSide * (doFront+doRear);
 	
-	modelLijst = randperm(segsPerMethod*numberOfMethods);
+	imageNameFront	= '../money/wholeplusborder/neur10/fit/f1.bmp';
+	imageNameRear	= '../money/wholeplusborder/neur10/fit/r1.bmp';
 
-	imageNameFront = '../money/wholeplusborder/neur10/fit/f1.bmp';
-	imageNameRear = '../money/wholeplusborder/neur10/fit/r1.bmp';
-
-	imageFront = imread(imageNameFront);
-	imageRear  = imread(imageNameRear);
+	imageFront		= imread(imageNameFront);
+	imageRear		= imread(imageNameRear);
 	
 	[frontY frontX] = size(imageFront);
 	[rearY rearX]   = size(imageRear);
 
-	segWidthFront = round(frontX/Xsegs);
-	segHeightFront = round(frontY/Ysegs);
-	segWidthRear = round(rearX/Xsegs);
-	segHeightRear = round(rearY/Ysegs);
+	segWidthFront   = round(frontX/Xsegs);
+	segHeightFront  = round(frontY/Ysegs);
+	segWidthRear    = round(rearX/Xsegs);
+	segHeightRear   = round(rearY/Ysegs);
 	
-	methodIdx   = ceil((bestModels./segsPerMethod));
-	frontOrRear = bestModels-((methodIdx-1)*segsPerMethod);
-	segInFront  = frontOrRear<=segsPerSide;
-	segInRear   = frontOrRear>segsPerSide;
-	frontSegs = frontOrRear(segInFront);
-	rearSegs = frontOrRear(segInRear);
-	frontMethod = methodIdx(segInFront);
-	rearMethod = methodIdx(segInRear);
+	methodIdx		= ceil((bestModels./segsPerMethod));
+	frontOrRear		= bestModels-((methodIdx-1)*segsPerMethod);
+	segInFront		= frontOrRear<=segsPerSide;
+	segInRear		= frontOrRear>segsPerSide;
+	frontSegs		= frontOrRear(segInFront);
+	rearSegs		= frontOrRear(segInRear);
+	frontMethod		= methodIdx(segInFront);
+	rearMethod		= methodIdx(segInRear);
+
+	colors = ['r','b'];
+
 	
 	figure(2)
 	subplot(1,2,1)
 	imshow(imageFront)
-	title('front    blue: edge, red: intensity')
+	title('front    red: edge, blue: intensity')
 	hold on
 	for i=1: length(frontSegs)
 		segment = frontSegs(i);
@@ -44,12 +45,11 @@ function [ output_args ] = IEshowAreasOnBill( Xsegs, Ysegs,...
 		end
 		x = ((m-1)*segWidthFront)+1;
 		y = ((n-1)*segHeightFront)+1;
-		if frontMethod(i)==1
-			color = 'b';
-		elseif frontMethod(i)==2
-			color = 'r';
-		end
-		rectangle('Position',[x,y,segWidthFront,segHeightFront],'EdgeColor',color)
+
+		rectangle('Position',[x,y,segWidthFront,segHeightFront],'EdgeColor',colors(frontMethod(i)))
+		p       = patch([x,x+segWidthFront,x+segWidthFront,x],...
+						[y,y,y+segHeightFront,y+segHeightFront] , colors(frontMethod(i)));
+		alpha(p , 0.3);
 	end
 	hold off
 	
@@ -67,13 +67,11 @@ function [ output_args ] = IEshowAreasOnBill( Xsegs, Ysegs,...
 		end
 		x = ((m-1)*segWidthRear)+1;
 		y = ((n-1)*segHeightRear)+1;
-		if rearMethod(i)==1
-			color = 'b';
-		elseif rearMethod(i)==2
-			color = 'r';
-		end
-		rectangle('Position',[x,y,segWidthRear,segHeightRear],'EdgeColor',color)
+		
+		rectangle('Position',[x,y,segWidthRear,segHeightRear],'EdgeColor',colors(rearMethod(i)))
+		p       = patch([x,x+segWidthRear,x+segWidthRear,x],...
+						[y,y,y+segHeightRear,y+segHeightRear] , colors(rearMethod(i)));
+		alpha(p , 0.3);
 	end
 	hold off
 end
-
