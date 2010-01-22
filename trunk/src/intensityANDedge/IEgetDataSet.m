57f0ac72f23e1c099ee66c3b01b830e22fbb875b
nraiman <nraiman@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-01-22 18:53:37 +0000

    directory renamed, color gone

---
+++ trunk/src/intensityANDedge/IEgetDataSet.m
@@ -0,0 +1,159 @@
+function [ allResults ] = IEgetDataSet( do, path,cannyThresh,...
+	useFront, useRear, invariant,xSegms, ySegms)
+
+	imgNr=1;
+	allResults=[0,0];
+	
+	if strcmp(path(length(path)-5:length(path)-1),'unfit')
+		dataset = 'unfit';
+	else
+		dataset = 'fit';
+	end
+
+	fprintf('\tconstructing the %s data set for %s\n',dataset, do)
+	maxImage=300;
+	for i = 1:maxImage
+		%for all images: extract the data
+
+		%construct front and rear image name
+		nextImageNameFront = [path 'f' num2str(i,'%01d') '.bmp'];
+		nextImageNameRear  = [path 'r' num2str(i,'%01d') '.bmp'];
+		%check if both exist in the dataset
+		if exist(nextImageNameFront,'file') &&...
+			exist(nextImageNameRear,'file')
+
+		  %load images as required in the initialization at the top
+			if (useFront)
+				nextImageFront = imread(nextImageNameFront);
+			end
+			if (useRear)
+				nextImageRear  = imread(nextImageNameRear);
+			end
+			imgSegFX = floor((size(nextImageFront,2)-1)/xSegms);
+			imgSegFY = floor((size(nextImageFront,1)-1)/ySegms);
+			imgSegRX = floor((size(nextImageRear,2)-1)/xSegms);
+			imgSegRY = floor((size(nextImageRear,1)-1)/ySegms);
+
+
+		  %get the data for classification
+		  if strcmp(do,'edge')
+			if (useFront)
+			  frontImageResults = doEdge(nextImageFront,cannyThresh,...
+				  invariant,imgSegFX,imgSegFY);
+			end
+			if (useRear)
+			  rearImageResults  = doEdge(nextImageRear,cannyThresh,...
+				  invariant,imgSegRX,imgSegRY);
+			end
+		  end
+		  if strcmp(do,'Intensity')
+			if (useFront)
+			  frontImageResults = doIntensity(nextImageFront,...
+				  invariant,imgSegFX,imgSegFY);
+			end
+			if (useRear)
+			  rearImageResults  = doIntensity(nextImageRear,...
+				  invariant,imgSegRX,imgSegRY);
+			end
+		  end
+
+% 		  if strcmp(do,'edge and Intensity')
+% 			if (useFront)
+% 			  frontImageResults = doEdgeIntensity(nextImageFront,...
+% 				  cannyThresh,invariant,imgSegFX,imgSegFY);
+% 			end
+% 			if (useRear)
+% 			  rearImageResults  = doEdgeIntensity(nextImageRear,...
+% 				  cannyThresh,invariant,imgSegRX,imgSegRY);
+% 			end
+% 		  end
+
+% 		  sizeFrontImageCount = size(frontImageResults)
+% 		  frontImageResults
+% 		  sizeRearImageCount = size(rearImageResults)
+% 		  rearImageResults
+		  %add count of front and rear image to results
+		  allResults(imgNr,1:xSegms*ySegms*2)=0;
+		  if (useFront)
+			insMin = 1;  
+			insMax = size(frontImageResults,2);  
+			allResults(imgNr,insMin:insMax) = frontImageResults;
+		  end
+		  if (useRear)
+			insMin = size(frontImageResults,2)+1;  
+			insMax = size(frontImageResults,2)+size(rearImageResults,2);  
+			allResults(imgNr,insMin:insMax) = rearImageResults;
+		  end
+		  
+		  %raise image count
+		  imgNr=imgNr+1;
+		end %if fileExists
+	end %for imgaes
+end
+
+function results = doEdge(image,cannyThresh,invariant,imgSegX, imgSegY)
+	if invariant==1
+		image = image - mean(mean(image));
+	end
+	edgeImage = edge(image,'canny',cannyThresh);
+	
+% 	edgeImage         = conv2(im2double(image), double([-1 1;-1 1]));
+% 	threshold = mean(mean(edgeImage));
+% 	filter            = (edgeImage<threshold);
+% 	edgeImage(filter) = 0;
+
+	%do per image segment and store in vector
+	results = [];
+	count=1;
+	for y=1:imgSegY:size(image,1)-imgSegY
+		for x=1:imgSegX:size(image,2)-imgSegX
+			if y+imgSegY+10>size(image,1)
+				untillY = size(image,1);
+			else
+				untillY = y+imgSegY-1;
+			end
+			if x+imgSegX+10>size(image,2)
+				untillX = size(image,2);
+			else
+				untillX = x+imgSegX-1;
+			end
+			results(count) = sum(sum(edgeImage(y:untillY,x:untillX)));
+			count = count+1;
+		end
+	end
+end
+
+function results = doIntensity(image,invariant,imgSegX, imgSegY)
+	if invariant==1
+		image = image - mean(mean(image));
+	end
+	%do per image segment and store in vector
+	results = [];
+	count=1;
+	for y=1:imgSegY:size(image,1)-imgSegY
+		for x=1:imgSegX:size(image,2)-imgSegX
+			if y+imgSegY+10>size(image,1)
+				untillY = size(image,1);
+			else
+				untillY = y+imgSegY-1;
+			end
+			if x+imgSegX+10>size(image,2)
+				untillX = size(image,2);
+			else
+				untillX = x+imgSegX-1;
+			end
+			results(count) = mean(mean(image(y:untillY,x:untillX)));
+			count = count+1;
+		end
+	end
+end
+
+% function count = doEdgeIntensity(image,cannyThresh,invariant,imgSegX, imgSegY)
+% 	if invariant==1
+% 		image = image - mean(mean(image));
+% 	end
+% 	factor = 10;
+% 	%average Intensity is raised by a factor to make it usefull for large count
+% 	%of edges. This might not be the ideal way to do this.
+% 	count = doEdge(image,cannyThresh) + (doIntensity(image)*factor);
+% end
---
+++ trunk/src/intensityANDedge/IEshowAreasOnBill.m
@@ -0,0 +1,79 @@
+function [ output_args ] = IEshowAreasOnBill( Xsegs, Ysegs,...
+	doFront,doRear,numberOfMethods,bestModels)
+
+	segsPerSide		= Xsegs * Ysegs;
+	segsPerMethod	= segsPerSide * (doFront+doRear);
+	
+	imageNameFront	= '../moneyDivided/wholeplusborder/neur05/fit/f2.bmp';
+	imageNameRear	= '../moneyDivided/wholeplusborder/neur05/fit/r2.bmp';
+
+	imageFront		= imread(imageNameFront);
+	imageRear		= imread(imageNameRear);
+	
+	[frontY frontX] = size(imageFront);
+	[rearY rearX]   = size(imageRear);
+
+	segWidthFront   = round(frontX/Xsegs);
+	segHeightFront  = round(frontY/Ysegs);
+	segWidthRear    = round(rearX/Xsegs);
+	segHeightRear   = round(rearY/Ysegs);
+	
+	methodIdx		= ceil((bestModels./segsPerMethod));
+	frontOrRear		= bestModels-((methodIdx-1)*segsPerMethod);
+	segInFront		= frontOrRear<=segsPerSide;
+	segInRear		= frontOrRear>segsPerSide;
+	frontSegs		= frontOrRear(segInFront);
+	rearSegs		= frontOrRear(segInRear);
+	frontMethod		= methodIdx(segInFront);
+	rearMethod		= methodIdx(segInRear);
+
+	colors = ['r','b'];
+
+	
+	figure(2)
+	subplot(1,2,1)
+	imshow(imageFront)
+	title('front    red: edge, blue: intensity')
+	hold on
+	for i=1: length(frontSegs)
+		segment = frontSegs(i);
+		m = mod(segment,Xsegs);
+		n = ((segment-m)/Xsegs)+1;
+		if m==0
+			m=Xsegs;
+			n=n-1;
+		end
+		x = ((m-1)*segWidthFront)+1;
+		y = ((n-1)*segHeightFront)+1;
+
+		rectangle('Position',[x,y,segWidthFront,segHeightFront],'EdgeColor',colors(frontMethod(i)))
+		p       = patch([x,x+segWidthFront,x+segWidthFront,x],...
+						[y,y,y+segHeightFront,y+segHeightFront] , colors(frontMethod(i)));
+		alpha(p , 0.3);
+%		set(p,'AlphaDataMapping', 0.2)	
+	end
+	hold off
+	
+	subplot(1,2,2)
+	imshow(imageRear)
+	title('rear')
+	hold on
+	for i=1: length(rearSegs)
+		segment = rearSegs(i) - segsPerSide;
+		m = mod(segment,Xsegs);
+		n = ((segment-m)/Xsegs)+1;
+		if m==0
+			m=Xsegs;
+			n=n-1;
+		end
+		x = ((m-1)*segWidthRear)+1;
+		y = ((n-1)*segHeightRear)+1;
+		
+		rectangle('Position',[x,y,segWidthRear,segHeightRear],'EdgeColor',colors(rearMethod(i)))
+		p       = patch([x,x+segWidthRear,x+segWidthRear,x],...
+						[y,y,y+segHeightRear,y+segHeightRear] , colors(rearMethod(i)));
+		alpha(p , 0.3);
+%		set(p,'AlphaDataMapping', 0.2)	
+	end
+	hold off
+end
