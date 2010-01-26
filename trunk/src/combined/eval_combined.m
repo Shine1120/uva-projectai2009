198672fd3c8b79c1794ef828634d17c032909ab1
nraiman <nraiman@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-01-26 17:17:53 +0000

    changes to combined evaluation

--- trunk/src/combined/eval_combined.m
+++ trunk/src/combined/eval_combined.m
@@ -1,10 +1,11 @@
 function eval_combined()
 
-	doread		= 1;
+	doread		= 0;
 	docreate	= 1;
 
 	moneydir = 'moneyDivided/wholeplusborderHO/neur10';
 
+	predictions = [];
 
 
 	if (doread)
@@ -15,26 +16,28 @@ function eval_combined()
 	load('all_money_rear');
 	load('all_money_front');		
 	load('all_labels')
-%	size(all_labels)
-%	size(all_money_front)
-	
 
-	predictions = final_eval(all_money_rear, all_money_front);
+	%haar evaluation
+% 	predictionsHaar = final_eval(all_money_front,all_money_rear);
+% 	predictions = [predictions predictionsHaar];
+
+	%Intensity & edges evaluation
+	predictionsIE = eval_IE(all_money_front, all_money_rear);
+	predictions = [predictions predictionsIE];
 	
-	size(predictions)
-	size(all_labels)
-	
-%	predictions = eval_pca(all_money_front, all_money_rear, all_labels, docreate);
-	
-	[all_labels predictions]
+	%pca evaluation
+%	predictionsPCA = eval_pca(all_money_front, all_money_rear, all_labels, docreate);
+%	predictions = [predictions predictionsPCA];
+
+	stop
 	
 	mask_fit = all_labels == 0;
 	mask_unfit = all_labels == 1;	
 	
 	error_unfit = 0;
-	good_unfit = 0;
-	error_fit = 0;	
-	good_fit = 0;
+	good_unfit  = 0;
+	error_fit   = 0;	
+	good_fit    = 0;
 
 	for i=1:length(all_labels)
 		if (all_labels(i) == predictions(i) && all_labels(i) == 1)
@@ -49,10 +52,10 @@ function eval_combined()
 	end
 	
 	error_unfit = error_unfit/sum(mask_unfit)
-	good_unfit = good_unfit/sum(mask_unfit)
+	good_unfit  = good_unfit/sum(mask_unfit)
 
-	error_fit = error_fit/sum(mask_fit)
-	good_fit = good_fit/sum(mask_fit)
+	error_fit   = error_fit/sum(mask_fit)
+	good_fit    = good_fit/sum(mask_fit)
 
 	
 	
