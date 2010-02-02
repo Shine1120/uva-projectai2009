406a4294df5f5a3b707afbda841c83dbff5d9b6e
jturnhout <jturnhout@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-02-02 16:05:13 +0000

    another update

--- src/pca/AdaBoostSVM.m
+++ src/pca/AdaBoostSVM.m
@@ -1,5 +1,15 @@
 function [alpha, modelIdx, models] = AdaBoostSVM(models, traindata, eigen_regions, labels, T)
+% Adaptive Boosting using SVM models as weak classifiers. Every model
+% represents a pca segment on the bill. 
+%
+% models:			SVM models trained on pca segments
+% traindata:		image train data in format [imgIdx, data, regionIdx]
+% eigen_regions:	components used for projection
+% labels:			labels of the traindata [0 1]
+% T:				number of models to find
 
+
+	% mask labels to have the set [-1 1] 
 	labels_masked = labels;
 	mask = labels == 0;
 	labels_masked(mask) = -1;
@@ -7,7 +17,7 @@ function [alpha, modelIdx, models] = AdaBoostSVM(models, traindata, eigen_region
 
 	alpha = zeros(1,T);
 	modelIdx = zeros(1,T);
-	
+
 	Dt = ones(1,m)*(1/m);
 
 	% get the best T models
@@ -17,16 +27,19 @@ function [alpha, modelIdx, models] = AdaBoostSVM(models, traindata, eigen_region
 		best_model = 1; % initialize to some model
 		smallest_error = 10000; % initialize error to some big number
 		
-		recognized_list = zeros(1,size(traindata,1));
 		best_recognized_list = [];
 		% find the best model by minimizing the error (with respect to Dt)
 		for j=1:length(models)
+			% get model of segment j
 			model = models(j);
-			error_j = 0;
 			
+			% get correct traindata for segment j
 			images_region = traindata(:,:,j);
+			
+			% get correct pca components for segment j
 			eigen_region = eigen_regions(:,:,j);
 			
+			% project traindata onto the components
 			imagesProjection = images_region * eigen_region;
 
 			[recognized, accuracy, prob_est_front] = svmpredict(labels,imagesProjection, model, '-b 0');
@@ -42,13 +55,13 @@ function [alpha, modelIdx, models] = AdaBoostSVM(models, traindata, eigen_region
 % 		fprintf('\tBest model for t=%d is %d\n',i, best_model)
 % 		fprintf('\t\tUpdate weights\n')
 		modelIdx(i) = best_model;
-		
+
 		% update alpha weights
 		alpha(i) = 0.5*log((1-smallest_error)/smallest_error); 
-		
+
 		% update instance weights
-		
-		
+
+
 		% TODO: in case the best_recognized_list is empty... how is this
 		% possible?
 		if numel(best_recognized_list) ~= 0
--- src/pca/genEigenFaceRegions.m
+++ src/pca/genEigenFaceRegions.m
@@ -1,12 +1,20 @@
 function eigen_regions = genEigenFaceRegions(img_regions)
+% genEigenFaceRegions calculates the components for each region on the
+% images
 
 	s = size(img_regions);
+
+	% allocate eigen_regions
 	eigen_regions = zeros(s(2),s(1),s(3));
 
 	for i=1:size(img_regions,3)
+		% get a specific region
 		img_region = img_regions(:,:,i);
+
+		% calculate components over this region
 		eigen_region = genEigenFaces(img_region);
-% 		size(eigen_region)
+
+		% store
 		eigen_regions(:,:,i) = eigen_region;
 	end
 
