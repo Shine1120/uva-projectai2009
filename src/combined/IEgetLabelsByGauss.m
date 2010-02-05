4ce8a46f83b625fffe86b19bce6fab98882bf542
nraiman <nraiman@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-02-05 00:00:03 +0000

    bla

--- src/combined/IEgetLabelsByGauss.m
+++ src/combined/IEgetLabelsByGauss.m
@@ -9,10 +9,21 @@ function [ labels ] = IEgetLabelsByGauss(data,models)
 	if ~isempty(data)
 		for x=1:size(models,2)
 			for y=1:size(data,1)
-				probBeFit   = mvnpdf(data(y,x),meanFit(x),covFit(x));
-				probBeUnfit = mvnpdf(data(y,x),meanUnfit(x),covUnfit(x));
+				%get the probability of the data belonging to fit or unfit
+				probBeFit      = mvnpdf(data(y,x),meanFit(x),covFit(x));
+				probBeUnfit    = mvnpdf(data(y,x),meanUnfit(x),covUnfit(x));
 
-				if probBeFit>probBeUnfit
+				%calculate the final probability using MAP
+				% prior probability for a fit bill is 60% 
+				% and for a unfit bill is 40%
+				sumProb = (0.6 * probBeFit + 0.4 * probBeUnfit);
+				finalFitProb   = (0.6 * probBeFit)/sumProb; 
+				finalUnfitProb = (0.4 * probBeUnfit)/sumProb; 
+
+				%if the probability for a fit bill is higher then the
+				%probability of an unfit bill label 1 is given to this
+				%data-item
+				if finalFitProb>finalUnfitProb
 					labels(y,x) = 0;
 				else
 					labels(y,x) = 1;
