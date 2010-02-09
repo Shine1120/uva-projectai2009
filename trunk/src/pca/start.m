3b2bcfdabb76335d8e0e3027a437739b2ac8fa15
jturnhout <jturnhout@1299d45a-fa36-11de-9e29-7d1cd92887ef>
2010-02-09 16:30:15 +0000

    pca code update

--- trunk/src/pca/start.m
+++ trunk/src/pca/start.m
@@ -1,6 +1,7 @@
+% Reading and creating are needed only once if moneydir is kept fixed.
 
-doread = 0;
-docreate = 0;
+doread = 1;
+docreate = 1;
 
 moneydir = '../moneyDivided/wholeplusborder/neur05';
 
@@ -11,7 +12,19 @@ if (doread)
 	preprocessnew(moneydir);
 end
 
+
+% In DNB_demo, all parameters for the experiment can be set.
 DNB_demo(docreate)
 
-
+% NOTE: since we are working with huge matrices, memory issues can occur.
+% In case for Windows, this can be solved by having Windows manage 
+% the virtual memory by itself.
+% My Computer -> 
+%	Properties -> Advanced -> 
+%		Performance Settings -> Advanced
+%			Virtual Memory -> Change
+%				Set System Managed Size
+%
+% Also keep track of what variables can be cleared and loaded during
+% execution.
 
